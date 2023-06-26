67a492b8eef47d1765cfa920dd422edfcb5f0961
Vitalii Denysenko <vytalyi.denysenko@gmail.com>
2023-06-26 21:51:58 +0200

    tailwing + nav skeleton added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vytalyi.github.io',
-    base: '/sandbox',  
+  site: 'https://vytalyi.github.io',
+  base: '/sandbox',
+  integrations: [tailwind()]
 });
---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface NavProps {
+    items: Array<NavLinkProps>
+}
+
+export interface NavLinkProps {
+    className: string,
+    url: string;
+    text: string;
+    active: boolean;
+}
+
+export type PageNameType = 'index' | 'dev'
