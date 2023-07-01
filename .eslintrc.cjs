39352fe4e3e236beefebe3f96be57516ad3eabe6
Vitalii Denysenko <vytalyi.denysenko@gmail.com>
2023-07-01 15:43:26 +0200

    move eslint + prettier configuration outside

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,60 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
-  overrides: [
-    {
-      files: ['*.astro'],
-      extends: ['plugin:astro/recommended'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-      rules: {
-        'astro/no-set-html-directive': 'error',
-        'astro/no-set-text-directive': 'error',
-        'astro/no-unused-css-selector': 'error',
-      },
-    },
-    {
-      files: ['*.js', '*.cjs', '*.mjs', '*.ts', '*.tsx'],
-      extends: ['plugin:react/recommended'],
-      plugins: ['react'],
-      rules: {
-        'react/jsx-wrap-multilines': [
-          'error',
-          {
-            declaration: 'parens-new-line',
-            assignment: 'parens-new-line',
-            return: 'parens-new-line',
-            arrow: 'parens-new-line',
-            condition: 'ignore',
-            logical: 'ignore',
-            prop: 'ignore',
-          },
-        ],
-        'react/react-in-jsx-scope': 'off',
-      },
-    },
-  ],
+  extends: ['eslint-config-vytalyi/eslint'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  rules: {
-    indent: 'off',
-    'no-tabs': 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/naming-convention': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/triple-slash-reference': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-  },
-};
+}
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,26 +1,3 @@
 module.exports = {
-  plugins: [require.resolve('prettier-plugin-astro')],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'none',
-      },
-    },
-    {
-      files: ['*.js', '*.cjs', '*.mjs', '*.ts', '*.tsx'],
-      options: {
-        semi: true,
-        tabWidth: 2,
-        printWidth: 100,
-        singleQuote: true,
-        trailingComma: 'all',
-        jsxSingleQuote: true,
-        bracketSpacing: true,
-      },
-    },
-  ],
+  ...require('eslint-config-vytalyi/prettier'),
 };
